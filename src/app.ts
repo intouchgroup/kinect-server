5b30567cec52a112f510e4697e65b17287e81718
Cody Nova <codypersinger@gmail.com>
2020-02-25 14:02:02 -0600

    Switch to single-file structure, send depth data to connected clients

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import Koa from 'koa';
 import logger from 'koa-logger';
 import cors from '@koa/cors';
 import bodyParser from 'koa-bodyparser';
-import errorHandler from './errorHandler';
-import router from './router';
+import Router from '@koa/router';
 import http from 'http';
 import Socket from 'socket.io';
+import KinectAzure from 'kinect-azure';
 
 const { PORT, CORS_DOMAIN } = process.env;
 const app = new Koa();
@@ -19,15 +19,46 @@ if (CORS_DOMAIN) {
 }
 
 app.use(bodyParser());
-app.use(errorHandler());
+
+app.use(async (ctx, next) => {
+    try {
+        await next();
+    }
+    catch (error) {
+        ctx.status = error.status || 500;
+        ctx.body = error.message;
+        ctx.app.emit('error', error, ctx);
+    }
+});
+
+const router = new Router();
+
+router.get('/', (async (ctx, next) => {
+    ctx.status = 200;
+    ctx.body = 'Kinect-ion made ;)';
+}) as Koa.Middleware);
+
+router.get('/kinect', (async (ctx, next) => {
+    ctx.status = 200;
+    ctx.body = 'Kinect data';
+}) as Koa.Middleware);
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 const server = http.createServer(app.callback());
 const io = Socket(server);
+const kinect = new KinectAzure();
 
-io.on('connection', socket => {
-    console.log('CLIENT CONNECTED TO SOCKET');
-});
+if (kinect.open()) {
+    kinect.startCameras({
+        depth_mode: KinectAzure.K4A_DEPTH_MODE_NFOV_UNBINNED,
+        color_resolution: KinectAzure.K4A_COLOR_RESOLUTION_1080P
+    });
+
+    kinect.createTracker();
+}
+
+io.on('connection', socket => kinect.startListening(data => socket.emit('kinectData', data.depthImageFrame)));
 
 server.listen(PORT || 3000);
